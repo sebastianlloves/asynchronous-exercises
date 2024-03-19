47dbe47480c5b6cd936214f8f4cf7bb5f7456c39
sebastianlloves <sebastianlloves@gmail.com>
2024-03-19 19:33:25 -0300

    Resolución ejercicio 14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,30 +1,43 @@
 // 14 - Realiza una llamada a una API con fetch() y, después de recibir los datos, encadena Promises para realizar varias operaciones de manipulación de datos, como filtrar, mapear o reducir los resultados.
 
-// Estos ejercicios deberían ayudar a los alumnos a practicar y reforzar los conceptos clave de la clase sobre Promises, async/await y llamadas a APIs en JavaScript.
-
-const URL = "https://jsonplaceholder.typicode.com/psts";
-
-const operation1 = (data) => data;
-const operation2 = (data) => data;
-const operation3 = (data) => data;
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const operation1 = (data) =>
+  data.map(({ title, body }) => {
+    return { title: title.toUpperCase(), body };
+  });
+const operation2 = (data) => data.filter((post) => post.body.startsWith("f"));
 
 // then()
 
 const getProcessDataWithThen = (url) => {
   fetch(url)
     .then((response) => {
-    //   if (response.status !== 200) throw new Error("Error de conexión");
+      if (!response.ok) throw new Error("Error de conexión");
       return response.json();
     })
     .then((data) => operation1(data))
     .then((data) => operation2(data))
-    .then((data) => operation3(data))
     .then((data) => console.log(data))
-    .catch((error) => console.log(`Se produjo un error del tipo: ${error.message}`))
-    .finally(() => console.log('Solicitud finalizada'));
+    .catch((error) =>
+      console.log(`Se produjo un error del tipo: ${error.message}`)
+    )
+    .finally(() => console.log("Solicitud finalizada"));
 };
 getProcessDataWithThen(URL);
 
 // async/await
 
-const getProcessDataWithAsyncAwait = (url) => {};
+const getProcessDataWithAsyncAwait = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Falló la conexión");
+    const data = await response.json();
+    const processData = operation2(operation1(data));
+    console.log(processData);
+  } catch (error) {
+    console.log(`Error mensaje: ${error.message}`);
+  } finally {
+    console.log("Solicitud finalizada");
+  }
+};
+getProcessDataWithAsyncAwait(URL);
